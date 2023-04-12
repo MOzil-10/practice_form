62935fc2f00c835625f9f40ed9cd5f60a5310666
trevor nkuna <trevornkuna7@gmail.com>
2023-04-12 12:04:08 +0200

    second commit

---
+++ register.js
@@ -0,0 +1,10 @@
+var firstName =  document.getElementById("fname");
+var lastName = document.getElementById("lname");
+var email = document.getElementById("email");
+
+function myFunction(){
+
+console.log(`First Name: ${firstName.value}`);
+console.log(`Last Name: ${lastName.value}`);
+console.log(`Email: ${email.value}`);
+}
